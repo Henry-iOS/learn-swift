9b623c121697192778be99d7db223a460bbe3042
Henry-iOS <60023501+Henry-iOS@users.noreply.github.com>
2020-01-18 14:46:06 -0800

    Ruff! I guess ..
    couldnt think of anything else to type here

---
+++ dogYears.swift
@@ -0,0 +1,10 @@
+var dogAge = 4
+// my dog is 4 years old
+
+var earlyYears = 21
+// Dog is 2 years old (10.5 *2)
+var laterYears = (dogAge - 2) * 4
+// take away early years for adjustment and any age above 2 when numbers change
+var humanYears = earlyYears + laterYears
+
+print ("my name is Midnight! Ruff ruff, i am [37] years old in human years")
